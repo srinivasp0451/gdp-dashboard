0226a41fa7d5b19f5788c6def006bf02cf682419
srinivasp0451 <80968477+srinivasp0451@users.noreply.github.com>
2024-10-23 07:22:23 +0530

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -44,15 +44,13 @@ def calculate_sma(data, window=50):
 def generate_signals(data):
     signals = []
     for i in range(len(data)):
-        rsi = data['RSI'].iloc[i]
-        macd = data['MACD'].iloc[i]
-        signal_line = data['Signal_Line'].iloc[i]
-        close_price = data['Close'].iloc[i]
-        sma_50 = data['SMA_50'].iloc[i]
-
-        if (rsi < 40) and (macd > signal_line) and (close_price > sma_50):
+        if (data['RSI'].iloc[i] < 40 and
+            data['MACD'].iloc[i] > data['Signal_Line'].iloc[i] and
+            data['Close'].iloc[i] > data['SMA_50'].iloc[i]):
             signals.append('Buy')
-        elif (rsi > 60) and (macd < signal_line) and (close_price < sma_50):
+        elif (data['RSI'].iloc[i] > 60 and
+              data['MACD'].iloc[i] < data['Signal_Line'].iloc[i] and
+              data['Close'].iloc[i] < data['SMA_50'].iloc[i]):
             signals.append('Sell')
         else:
             signals.append('Hold')
