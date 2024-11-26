e5baf240a5c8b29d8b1a7d8167fbf437f56aa5c0
srinivasp0451 <80968477+srinivasp0451@users.noreply.github.com>
2024-11-26 09:22:17 +0530

    Update test000111.py

--- test000111.py
+++ test000111.py
@@ -155,8 +155,8 @@ def main():
 
     # Backtesting settings
     if strategy_type == "Backtesting":
-        period = st.selectbox("Select Period", ["1d", "5d", "1mo", "3mo", "6mo","1y","2y","5y"], index=3)
-        interval = st.selectbox("Select Interval", ["1m", "2m", "5m", "15m", "30m", "60m","1wk"], index=2)
+        period = st.selectbox("Select Period", ["1d", "5d", "1mo", "3mo", "6mo","1y","2y","5y"], index=2)
+        interval = st.selectbox("Select Interval", ["1m", "2m", "5m", "15m", "30m", "60m","1wk"], index=3)
         stoploss_points = st.number_input("Enter Stop Loss (Points)", min_value=1, value=10)
         
         # Button to run backtest
