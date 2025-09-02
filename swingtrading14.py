88de55c575946461efc6385398ed91c9bcd399ab
srinivasp0451 <80968477+srinivasp0451@users.noreply.github.com>
2025-09-03 00:14:12 +0530

    Update swingtrading14.py

--- swingtrading14.py
+++ swingtrading14.py
@@ -14,15 +14,15 @@ def normalize_df(df):
     cols = {c.lower(): c for c in df.columns}
     mapping = {}
     for key, orig in cols.items():
-        if key in ("open", "o"):
+        if key in ("open"):
             mapping[orig] = "Open"
-        if key in ("high", "h"):
+        if key in ("high"):
             mapping[orig] = "High"
-        if key in ("low", "l"):
+        if key in ("low"):
             mapping[orig] = "Low"
-        if key in ("close", "c", "price"):
+        if key in ("close"):
             mapping[orig] = "Close"
-        if key in ("volume", "vol", "v"):
+        if key in ("volume", "vol"):
             mapping[orig] = "Volume"
         if key in ("date", "datetime", "time"):
             mapping[orig] = "Date"
