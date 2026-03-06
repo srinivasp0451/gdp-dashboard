3ae1cce8d093da95537acc4a57f1c1acd996058b
srinivasp0451 <80968477+srinivasp0451@users.noreply.github.com>
2026-03-06 09:14:44 +0530

    Update swingtrading74.py

--- swingtrading74.py
+++ swingtrading74.py
@@ -4191,7 +4191,7 @@ def render_config_ui():
     
     if config['dhan_enabled']:
         config['dhan_client_id'] = st.sidebar.text_input("Client ID", value="1104779876")
-        config['dhan_access_token'] = st.sidebar.text_input("Access Token", type="password", value="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzcyNzY4OTU4LCJpYXQiOjE3NzI2ODI1NTgsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA0Nzc5ODc2In0.tc4RYV8daLjGe1AaLYjr-sGhJ7AnnkdbB7ZTMp1WSl6qJIM_T05eq-6E-BA3YTdQZBh3KEV-M6uj6Hf_JA3ehA")
+        config['dhan_access_token'] = st.sidebar.text_input("Access Token", type="password", value="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzcyODU0OTQyLCJpYXQiOjE3NzI3Njg1NDIsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA0Nzc5ODc2In0.oKRtH3L4DVuESTNjENUlqEavKveoAqO33psy1Hp4fLjJohh5aZQCjrp982J7fZpG4evMQOV94q3xBIfgyBZkSA")
         
         config['dhan_is_options'] = st.sidebar.checkbox("Is Options", value=True)
         
